165953fa082fc175221c5bf3f0484d4292f9db59
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-10-05 19:59:31 +0530

    Refactor ESLint configuration for improved readability and consistency

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  parserOptions: {
-    "ecmaVersion": 2018,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "single", {"allowTemplateLiterals": true}],
-  },
-  overrides: [
-    {
-      files: ["**/*.spec.*"],
-      env: {
-        mocha: true,
-      },
-      rules: {},
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
     },
-  ],
-  globals: {},
+    parserOptions: {
+        "ecmaVersion": 2018,
+    },
+    extends: [
+        "eslint:recommended",
+        "google",
+    ],
+    rules: {
+        "no-restricted-globals": ["error", "name", "length"],
+        "prefer-arrow-callback": "error",
+        "quotes": ["error", "single", { "allowTemplateLiterals": true }],
+    },
+    overrides: [
+        {
+            files: ["**/*.spec.*"],
+            env: {
+                mocha: true,
+            },
+            rules: {},
+        },
+    ],
+    globals: {},
 };
